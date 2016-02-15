d7d650290cb7db7c2be31899df1f85b3961d96f7
AntikCz <antik494@gmail.com>
2016-02-15 17:07:33 +0100

    Added travis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/WebChemistry/Images.svg?branch=master)](https://travis-ci.org/WebChemistry/Images)
+
 ## Installation
 
 Composer:
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../../../autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 \Codeception\Util\Autoload::addNamespace('WebChemistry\Images', __DIR__ . '/../src');
 \Codeception\Util\Autoload::addNamespace('', __DIR__ . '/includes');
