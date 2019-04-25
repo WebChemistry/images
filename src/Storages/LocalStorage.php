2995a029b005445b55a949e40e9a2367d3adaafc
MartkCz <antik494@gmail.com>
2019-04-25 18:10:50 +0200

    LocalStorage pass original image path

--- src/Storages/LocalStorage.php
+++ src/Storages/LocalStorage.php
@@ -132,6 +132,7 @@ class LocalStorage extends Storage {
 	 */
 	protected function getLink(IResource $resource): ?string {
 		$meta = $this->metaFactory->create($resource);
+		$originalPath = null;
 
 		if ($resource instanceof ITransferResource) {
 			$resource->setSaved();
@@ -173,7 +174,7 @@ class LocalStorage extends Storage {
 			throw new ImageStorageException('Resource must be instance of ITransferResource or IFileResource.');
 		}
 
-		$meta->modify($image, $this->getResourceLocation($meta));
+		$meta->modify($image, $originalPath);
 		$this->makeDir($path);
 		$image->save($path);
 
